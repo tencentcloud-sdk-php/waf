0fbd6ce0be026d4d789c66ff0cc63cf73fab123e
tencentcloudapi <tencentcloudapi@tencent.com>
2022-07-20 07:27:57 +0800

    release 3.0.675

--- src/TencentCloud/Waf/V20180125/Models/PostAttackDownloadTaskResponse.php
+++ src/TencentCloud/Waf/V20180125/Models/PostAttackDownloadTaskResponse.php
@@ -18,19 +18,19 @@ namespace TencentCloud\Waf\V20180125\Models;
 use TencentCloud\Common\AbstractModel;
 
 /**
- * GetAttackDownloadRecords返回参数结构体
+ * PostAttackDownloadTask返回参数结构体
  *
- * @method array getRecords() 获取下载攻击日志记录数组
- * @method void setRecords(array $Records) 设置下载攻击日志记录数组
+ * @method string getFlow() 获取任务task id
+ * @method void setFlow(string $Flow) 设置任务task id
  * @method string getRequestId() 获取唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
  */
-class GetAttackDownloadRecordsResponse extends AbstractModel
+class PostAttackDownloadTaskResponse extends AbstractModel
 {
     /**
-     * @var array 下载攻击日志记录数组
+     * @var string 任务task id
      */
-    public $Records;
+    public $Flow;
 
     /**
      * @var string 唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
@@ -38,7 +38,7 @@ class GetAttackDownloadRecordsResponse extends AbstractModel
     public $RequestId;
 
     /**
-     * @param array $Records 下载攻击日志记录数组
+     * @param string $Flow 任务task id
      * @param string $RequestId 唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
@@ -54,13 +54,8 @@ class GetAttackDownloadRecordsResponse extends AbstractModel
         if ($param === null) {
             return;
         }
-        if (array_key_exists("Records",$param) and $param["Records"] !== null) {
-            $this->Records = [];
-            foreach ($param["Records"] as $key => $value){
-                $obj = new DownloadAttackRecordInfo();
-                $obj->deserialize($value);
-                array_push($this->Records, $obj);
-            }
+        if (array_key_exists("Flow",$param) and $param["Flow"] !== null) {
+            $this->Flow = $param["Flow"];
         }
 
         if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
